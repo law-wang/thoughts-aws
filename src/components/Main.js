d13b1251dc383fe811ec928c2cc1f14dbc042af9
law-wang <47406212+law-wang@users.noreply.github.com>
2022-04-14 12:36:47 -0400

    working push

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,47 +11,48 @@ import '../style.css'
 
 function Main() {
 
-  const [isAdmin, setIsAdmin] = useState(false)
+    const [user, setUser] = useState({})
+    const [isAdmin, setIsAdmin] = useState(false)
 
-  useEffect(() => {
-    const getData = async () => {
-        try {        
-            // fetch the current signed in user
-            // then, check to see if they're a member of the admin user group
-            const user = await Auth.currentAuthenticatedUser()
-            setIsAdmin(user.signInUserSession.accessToken.payload['cognito:groups'].includes('admin'))
-            setUser(user)
+    useEffect(() => {
+        const getData = async () => {
+            try {        
+                // fetch the current signed in user
+                // then, check to see if they're a member of the admin user group
+                const user = await Auth.currentAuthenticatedUser()
+                setIsAdmin(user.signInUserSession.accessToken.payload['cognito:groups'].includes('admin'))
+                setUser(user)
 
-        } catch (err) {
-            console.error(err)
+            } catch (err) {
+                console.error(err)
+            }
+        }    
+
+        const listener = Hub.listen("datastore", async hubData => {
+            console.log("start")
+            const  { event, data } = hubData.payload;
+            console.log(event)
+            console.log(data)
+            if (event === "ready") {
+                console.log("ready")
+                // do something here once the data is synced from the cloud
+            }
+        })
+
+        getData()
+
+        return () => {
+            listener()
         }
-    }    
-    const listener = Hub.listen("datastore", async hubData => {
-        console.log("start")
-        const  { event, data } = hubData.payload;
-        console.log(event)
-        console.log(data)
-        if (event === "ready") {
-            console.log("ready")
-            // do something here once the data is synced from the cloud
-        }
-    })
+    }, [])    
 
-    getData()
-
-
-    return () => {
-        listener()
-    }
-  }, [])
-
-  return (
-    <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/create' element={<Create user={isAdmin} />} />
-        <Route exact path='/' element={<Blog />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/create' element={<Create user={isAdmin} />} />
+            <Route exact path='/' element={<Blog />} />
+        </Routes>
+    )
 }
 
 export default Main
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom"
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
 
 import Amplify from "@aws-amplify/core"
 import { AuthModeStrategyType } from 'aws-amplify'
