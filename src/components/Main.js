9f3e3d056e483385c8ef8de4acbbeb614505aaa0
rencewang <47406212+rencewang@users.noreply.github.com>
2022-11-04 21:32:11 -0400

    check for user status -- try to fix safari content not loading problem

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,43 +1,48 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Auth } from 'aws-amplify'
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
 
-import Login from './Auth'
-import Blog from './Blog'
-import Create from './Create'
+import Login from './Auth';
+import Blog from './Blog';
+import Create from './Create';
 
-import '../style.css'
+import '../style.css';
 
 function Main() {
+  const [user, setUser] = useState({});
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    const [user, setUser] = useState({})
-    const [isAdmin, setIsAdmin] = useState(false)
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        // fetch the current signed in user
+        // then, check to see if they're a member of the admin user group
+        const user = await Auth.currentAuthenticatedUser();
+        if (user) {
+          // const userInfo = { username: user.username, ...user.attributes }
+          // setUser(userInfo)
+          setUser(user);
+          setIsAdmin(
+            user.signInUserSession.accessToken.payload[
+              'cognito:groups'
+            ].includes('admin')
+          );
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-    useEffect(() => {
-        const getData = async () => {
-            try {        
-                // fetch the current signed in user
-                // then, check to see if they're a member of the admin user group
-                const user = await Auth.currentAuthenticatedUser()
-                setIsAdmin(user.signInUserSession.accessToken.payload['cognito:groups'].includes('admin'))
-                setUser(user)
+    getData();
+  }, []);
 
-            } catch (err) {
-                console.error(err)
-            }
-        }    
-
-        getData()
-
-    }, [])    
-
-    return (
-        <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/create' element={<Create user={isAdmin} />} />
-            <Route exact path='/' element={<Blog />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/create" element={<Create user={isAdmin} />} />
+      <Route exact path="/" element={<Blog />} />
+    </Routes>
+  );
 }
 
-export default Main
+export default Main;
